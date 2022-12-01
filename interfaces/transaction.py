194e7738bc9df2aee163e0d096c7b135757844aa
vladddd46 <vladddd46@ukr.net>
2022-12-01 15:33:53 +0200

    improved interfaces

--- interfaces/transaction.py
+++ interfaces/transaction.py
@@ -1,15 +1,32 @@
 import json
 
-class TransactionTypes:
-	login = 1
-	logout = 2
-	send_money = 3
-	get_cash = 4
-	change_password = 5
-	check_balance = 6
+
+class TransactionType:
+    send_money = 1
+    get_cash = 2
+    check_balance = 4
+
+
+class Currency:
+    USD = 1
+    UAH = 2
+    EUR = 3
+
 
 class Transaction:
+    def __init__(self, ttype=None, amout=None, currency=None, receiver=None):
+        self.ttype = ttype
+        self.amout = amout
+        self.currency = currency
+        self.receiver = receiver
 
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
-	def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__,  sort_keys=True, indent=4)
+    def from_json(self, json_str):
+        data = json.loads(json_str)
+        self.__dict__ = data
+        return self
+
+    def __str__(self):
+        return f"\nTransaction:\n\ttype={self.ttype}\n\tamout={self.amout}\n\tcurrency={self.currency}\n\treceiver={self.receiver}\n\t"
