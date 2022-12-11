983d8e078942ce1503e2a79d74310d447efb3fd7
vladddd46 <vladddd46@ukr.net>
2022-12-11 22:23:49 +0200

    refactored

--- client/communicator.py
+++ client/communicator.py
@@ -2,35 +2,47 @@ import socket
 
 
 class Communicator:
-    """
-    # Responsible for making requests to server and getting response
-    """
+	"""
+	# Responsible for making requests to server and getting response
+	"""
 
-    def __init__(self, ip="127.0.0.1", port=5000):
-        self.ip = ip
-        self.port = int(port)
+	def __init__(self, ip="127.0.0.1", port=5000):
+		self.ip = ip
+		try:
+			self.port = int(port)
+		except Exception as e:
+			print(e)
+			print("port is wrong")
 
-    def __recv_data_from_sock(self, sock):
-        """
-        # receives all data from socket <sock>
-        """
-        BUFF_SIZE = 4096
-        data = b""
-        while True:
-            part = sock.recv(BUFF_SIZE)
-            data += part
-            if len(part) < BUFF_SIZE:
-                break
-        return data
+	def __recv_data_from_sock(self, sock):
+		"""
+		# receives all data from socket <sock>
+		"""
+		BUFF_SIZE = 4096
+		data = b""
+		while True:
+			part = sock.recv(BUFF_SIZE)
+			data += part
+			if len(part) < BUFF_SIZE:
+				break
+		return data
 
-    def make_request(self, request):
-        """
-        # sends request -> gets response from server.
-        """
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((self.ip, self.port))
-        client_socket.sendall(bytes(request, "utf-8"))
-        data = self.__recv_data_from_sock(client_socket)
-        data = data.decode("utf-8")
-        client_socket.close()
-        return data
+	def make_request(self, request):
+		"""
+		# sends request -> gets response from server.
+		"""
+		client_socket = None
+		try:
+			client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			client_socket.connect((self.ip, self.port))
+			client_socket.sendall(bytes(request, "utf-8"))
+			data = self.__recv_data_from_sock(client_socket)
+			data = data.decode("utf-8")
+			client_socket.close()
+		except Exception as e:
+			print(e)
+			print("Error during request")
+			if client_socket:
+				client_socket.close()
+			exit(1)
+		return data
--- client/gui.py
+++ client/gui.py
@@ -6,43 +6,79 @@ import hashlib
 from user import AuthorizedUser
 from communicator import Communicator
 
-class ScreenType:
-	entry = 0
-	invalid_card = 1
-	pin_code = 2
-	invalid_pin = 3
-	options = 4
-	options_with_label = 5
-
-
 class Gui:
+
+	class ScreenType:
+		entry = 0
+		invalid_card = 1
+		pin_code = 2
+		invalid_pin = 3
+		options = 4
+		options_with_label = 5
+
 	def __init__(
 		self, title, win_size, bg_img=None, is_resizableX=False, is_resizeableY=False
 	):
+		# setting up window
 		self.window = Tk()
-		self.title = title
-		self.is_resizableX = is_resizableX
-		self.is_resizeableY = is_resizeableY
+		self.window.title(title)
+		self.window.geometry(f"{win_size}x{win_size}")
+		self.window.resizable(is_resizableX, is_resizeableY)
 		self.win_size = win_size
-		self.elements_to_draw = {}
-		self.pin_code_input = ""
-		self.user_card = None
-		self.auth_user = None
+		#
+		# user inputs
+		self.pin_input = ""
 		self.amount_input = None
-		self.send_card_input = None
-		self.set_background_image(bg_img)
-		self.__apply_settings()
+		self.receiver_card_input = None
+		#
+		# elements (btns/lables/etc), that are drawn on window
+		self.elems_to_draw = {}
+		#
+		# inserted by user card. dict object. contains info about user card.
+		self.user_card = None
+		#
+		# AuthorizedUser object, which is created after user inserts card and enters correct pin.
+		# Is used for making queries to server in order to make operations [send_money/get_cash/check_balance]
+		self.auth_user = None
+		#
+		# background image
+		self.bg_img = bg_img
+		self.set_background(bg_img)
 
-	def __apply_settings(self):
-		self.window.title(self.title)
-		self.window.geometry(f"{self.win_size}x{self.win_size}")
-		self.window.resizable(self.is_resizableX, self.is_resizeableY)
+
+	def __clear_screen(self):
+		# empties elems_to_draw, so no elements are drawn.
+		self.elems_to_draw = {}
+
+	def __add_label(self, x=0, y=0, text="", fg="black", bg="white", font_size=25):
+		lbl = Label(self.window, text=text, fg=fg, bg=bg, font=("Helvetica", font_size))
+		self.elems_to_draw[lbl] = {"x": x, "y": y}
+		return lbl
+
+	def __add_button(self, x, y, text, h, w, callback):
+		btn = Button(self.window,text=text,height=h,width=w,command=callback)
+		self.elems_to_draw[btn] = {"x": x, "y": y}
+		return btn
+
+	def set_background(self, img_path=None):
+		if img_path:
+			self.bg_img = ImageTk.PhotoImage(Image.open(img_path))
+
+	def __render_background_img(self, x=0, y=0):
+		bg = Label(self.window, image=self.bg_img)
+		self.elems_to_draw[bg] = {"x": x,"y": y}
+		return self.elems_to_draw
 
 	def __insert_card_callback(self):
-		file_path = filedialog.askopenfilename(initialdir="../card_simulation", title="Select a File")
+		"""
+		# is called, when user press button "insert card".
+		# invoke user to insert card(attach file, that represents card)
+		"""
 		try:
+			file_path = filedialog.askopenfilename(initialdir="../card_simulation", title="Select a File")
 			with open(file_path, "r") as fd:
 				fdata = json.loads(fd.read())
+				# checks signature in order to check, taht card is valid.
 				if fdata["signature"] != hashlib.md5(b'goodbankofindia').hexdigest():
 					raise Exception("Signature is wrong")
 				self.user_card = fdata 
@@ -53,111 +89,59 @@ class Gui:
 			self.__clear_screen()
 			self.render_invalid_card_screen();
 
-	def __render_background_img(self, x=0, y=0):
-		background = Label(self.window, image=self.bg_img)
-		self.elements_to_draw[background] = {
-			"x": x,
-			"y": y,
-		}
-		return self.elements_to_draw
-
-	def set_background_image(self, img_path=None):
-		if img_path:
-			self.bg_img = ImageTk.PhotoImage(Image.open(img_path))
-
-	def __clear_screen(self):
-		self.elements_to_draw = {}
-
 	def render_entry_screen(self):
-		self.set_background_image(img_path="./resources/bg.png")
+		self.set_background(img_path="./resources/bg.png")
 		self.__render_background_img(x=-10, y=-10)
-		# Text labels
-		welcome_text = Label(
-			self.window, text="Hello guys,\nWelcome to Good Bank Of India", fg="black", font=("Helvetica", 20)
-		)
-		self.elements_to_draw[welcome_text] = {
-			"x": 120,
-			"y": int(self.win_size * 0.182),
-		}
-
-		# button to insert card
-		button_explore = Button(
-			self.window,
-			text="<insert card>",
-			height=1,
-			width=28,
-			command=self.__insert_card_callback,
-		)
-		self.elements_to_draw[button_explore] = {
-			"x": int(self.win_size * 0.273),
-			"y": int(int(self.win_size * 0.182) * 2.5),
-		}
-		return self.elements_to_draw
+		self.__add_label(x=120, y=92, text="Hello guys,\nWelcome to Good Bank Of India", font_size=20)
+		self.__add_button(x=139, y=230, text="<insert card>", h=1, w=28, callback=self.__insert_card_callback)
+		return self.elems_to_draw
 
 	def render_invalid_card_screen(self):
 		self.render_entry_screen()
-		invalid_card_msg = Label(
-			self.window, text="invalid card...", fg="red", font=("Helvetica", 20)
-		)
-		self.elements_to_draw[invalid_card_msg] = {
-			"x": int(self.win_size * 0.40),
-			"y": 170,
-		}
-		return self.elements_to_draw
+		self.__add_label(x=204, y=170, text="invalid card...", fg="red", font_size=20)
+		return self.elems_to_draw
 
-	def pincode_press(self, btn_text):
+	def pincode_btn_press_callback(self, btn_text):
 		if btn_text == "BACK":
 			self.__exit()
-		elif btn_text == "cancel" and len(self.pin_code_input)>0:
-			self.pin_code_input = self.pin_code_input[0:-1]
-			display_pin = "*" * len(self.pin_code_input)
+		elif btn_text == "cancel" and len(self.pin_input)>0:
+			self.pin_input = self.pin_input[0:-1]
 			self.__clear_screen()
+			display_pin = "*" * len(self.pin_input)
 			self.render_pincode_enter_screen(pincode_lbl_text=display_pin)
 		elif btn_text == "ENTER":
-			if self.user_card["pincode"] == hashlib.md5(self.pin_code_input.encode()).hexdigest():
-				com = Communicator(ip="127.0.0.1", port=sys.argv[1])
+			if self.user_card["pincode"] == hashlib.md5(self.pin_input.encode()).hexdigest():
+				port = int(sys.argv[1])
+				com = Communicator(ip="127.0.0.1", port=port)
 				self.auth_user = AuthorizedUser(com, self.user_card["number"], self.user_card["cvv"], self.user_card["date"])
 				self.__clear_screen()
 				self.render_options_screen()
 			else:
+				# inserted card is invalid => signature is incorrect.
 				self.render_invalid_pin_screen()
-		elif btn_text in "#*0123456789" and len(self.pin_code_input) < 4:
-			self.pin_code_input += btn_text
-			display_pin = "*" * len(self.pin_code_input)
+		elif btn_text in "#*0123456789" and len(self.pin_input) < 4:
+			self.pin_input += btn_text
 			self.__clear_screen()
+			display_pin = "*" * len(self.pin_input)
 			self.render_pincode_enter_screen(pincode_lbl_text=display_pin)
 
 
-	def add_button(self, ch, x, y, h, w):
+	def add_pincode_button(self, ch, x, y, h, w):
 		button_explore = Button(
 					self.window,
 					text=ch,
 					height=h,
 					width=w,
-					command=lambda: self.pincode_press(ch),
+					command=lambda: self.pincode_btn_press_callback(ch),
 				)
-		self.elements_to_draw[button_explore] = {
-			"x": x,
-			"y": y,
-		}
+		self.elems_to_draw[button_explore] = {"x": x,"y": y}
 
 	def render_pincode_enter_screen(self, pincode_lbl_text="____",pinlabel_bg_color="white"):
-		self.set_background_image(img_path="./resources/bg.png")
+		self.set_background(img_path="./resources/bg.png")
 		self.__render_background_img(x=-10, y=-10)
-
 		if len(pincode_lbl_text) < 4:
 			pincode_lbl_text += "_" * ((len(pincode_lbl_text)-4) * -1)
-		pincode_text = Label(
-			self.window,
-			text=f"  {pincode_lbl_text[0]}  {pincode_lbl_text[1]}  {pincode_lbl_text[2]}  {pincode_lbl_text[3]}  ",
-			fg="black",
-			bg=pinlabel_bg_color,
-			font=("Helvetica", 40),
-		)
-		self.elements_to_draw[pincode_text] = {
-			"x": int(self.win_size * 0.29),
-			"y": int(self.win_size * 0.15),
-		}
+		self.__add_label(x=147, y=76, text=f"  {pincode_lbl_text[0]}  {pincode_lbl_text[1]}  {pincode_lbl_text[2]}  {pincode_lbl_text[3]}  ", fg="black", bg=pinlabel_bg_color, font_size=40)
 		template = [
 			["#", "*", "cancel"],
 			["1", "2", "3"],
@@ -171,15 +155,15 @@ class Gui:
 		for row in range(len(template)):
 			x_offset=0
 			for column in range(len(template[row])):
-				self.add_button(template[row][column],x_pos+x_offset,y_pos+y_offset,2,5)
+				self.add_pincode_button(template[row][column],x_pos+x_offset,y_pos+y_offset,2,5)
 				x_offset += 60
 			y_offset += 50
-		return self.elements_to_draw
+		return self.elems_to_draw
 
 	def render_invalid_pin_screen(self):
-		display_pin = "*" * len(self.pin_code_input)
+		display_pin = "*" * len(self.pin_input)
 		self.render_pincode_enter_screen(pinlabel_bg_color="red", pincode_lbl_text=display_pin)
-		return self.elements_to_draw
+		return self.elems_to_draw
 
 	def __check_balance(self):
 		response = self.auth_user.get_balance()
@@ -205,35 +189,15 @@ class Gui:
 	def __get_cash(self):
 		self.__clear_screen()
 		self.render_options_screen()
-		
-		amount_lbl = Label(
-			self.window, text="Enter amount:", fg="black", font=("Helvetica", 20)
-		)
-		self.elements_to_draw[amount_lbl] = {
-			"x": 250,
-			"y": 60,
-		}
+		self.__add_label(x=250, y=60, text="Enter amount:", fg="black", bg="white", font_size=20)
 		self.amount_input = Entry(self.window, width=20)
 		self.amount_input.focus_set()
-		self.elements_to_draw[self.amount_input] = {
-			"x": 250,
-			"y": 100,
-		}
-		get_btn = Button(
-				self.window,
-				text="get",
-				height=2,
-				width=10,
-				command=self.__get_cash_callback,
-		)
-		self.elements_to_draw[get_btn] = {
-			"x": 250,
-			"y": 150,
-		}
+		self.elems_to_draw[self.amount_input] = {"x": 250,"y": 100}
+		self.__add_button(250, 150, "get", 2, 10, self.__get_cash_callback)
 
 
 	def __send_money_callback(self):
-		receiver_card_num = self.send_card_input.get()
+		receiver_card_num = self.receiver_card_input.get()
 		if len(receiver_card_num) != 16:
 			self.__clear_screen()
 			self.render_options_with_label_screen(label_msg=f"incorrect receiver card", x=225, y=100, font_size=20)
@@ -243,8 +207,7 @@ class Gui:
 				self.__clear_screen()
 				self.render_options_with_label_screen(label_msg=f"incorrect receiver card", x=225, y=100, font_size=20)
 				return
-		receiver = receiver_card_num[0:4] + "." + receiver_card_num[4:8] + "." + receiver_card_num[8:12] + "." + receiver_card_num[12:16]		
-		print(receiver)
+		receiver = receiver_card_num[0:4]+"."+receiver_card_num[4:8]+"."+receiver_card_num[8:12]+"."+receiver_card_num[12:16]
 		try:
 			amount_to_send = float(self.amount_input.get())
 		except:
@@ -258,7 +221,6 @@ class Gui:
 			self.render_options_with_label_screen(label_msg=f"not enough money", x=225, y=100, font_size=25)
 			return;
 		response = json.loads(self.auth_user.send_money(receiver=receiver, amount=amount_to_send))
-		
 		if response["data"]["retcode"] == 1:
 			self.render_options_with_label_screen(label_msg=response["data"]["msg"], x=225, y=100, font_size=25)
 			return;
@@ -269,56 +231,27 @@ class Gui:
 	def __send_money(self):
 		self.__clear_screen()
 		self.render_options_screen()
-		
-		send_card_lbl = Label(
-			self.window, text="Card to send money:", fg="black", font=("Helvetica", 20)
-		)
-		self.elements_to_draw[send_card_lbl] = {
-			"x": 250,
-			"y": 0,
-		}
-		self.send_card_input = Entry(self.window, width=20)
-		self.send_card_input.focus_set()
-		self.elements_to_draw[self.send_card_input] = {
-			"x": 250,
-			"y": 40,
-		}
-
-		amount_lbl = Label(
-			self.window, text="Enter amount:", fg="black", font=("Helvetica", 20)
-		)
-		self.elements_to_draw[amount_lbl] = {
-			"x": 250,
-			"y": 80,
-		}
+		self.__add_label(x=250, y=0, text="Card to send money:", fg="black", font_size=20)
+		self.receiver_card_input = Entry(self.window, width=20)
+		self.receiver_card_input.focus_set()
+		self.elems_to_draw[self.receiver_card_input] = {"x": 250,"y": 40}
+		self.__add_label(x=250, y=80, text="Enter amount:", fg="black", font_size=20)
 		self.amount_input = Entry(self.window, width=20)
 		self.amount_input.focus_set()
-		self.elements_to_draw[self.amount_input] = {
-			"x": 250,
-			"y": 120,
-		}
-		send_btn = Button(
-				self.window,
-				text="send",
-				height=2,
-				width=10,
-				command=self.__send_money_callback,
-		)
-		self.elements_to_draw[send_btn] = {
-			"x": 250,
-			"y": 180,
-		}
+		self.elems_to_draw[self.amount_input] = {"x": 250,"y": 120}
+		self.__add_button(250, 180, "send", 2, 10, self.__send_money_callback)
 
 	def __exit(self):
-		# goes to enrty screen
-		self.pin_code_input = ""
+		self.pin_input = ""
+		self.amount_input = None
+		self.receiver_card_input = None
 		self.user_card = None
 		self.auth_user = None
 		self.__clear_screen()
 		self.render_entry_screen()
 
 	def render_options_screen(self):
-		self.set_background_image(img_path="./resources/wxp.jpeg")
+		self.set_background(img_path="./resources/wxp.jpeg")
 		self.__render_background_img(x=-5, y=-5)
 		options = {
 			"check balance": self.__check_balance,
@@ -326,66 +259,45 @@ class Gui:
 			"send money": self.__send_money,
 			"exit": self.__exit,
 		}
-
-		distance = 0
+		y = 0
 		for option, callback in options.items():
-			button_explore = Button(
-				self.window,
-				text=option,
-				height=3,
-				width=15,
-				command=callback,
-			)
-			self.elements_to_draw[button_explore] = {
-				"x": 0,
-				"y": 0 + distance,
-			}
-			distance += 70
-		return self.elements_to_draw
+			self.__add_button(0, y, option, 3, 15, callback)
+			y += 70
+		return self.elems_to_draw
 
 	def render_options_with_label_screen(self, label_msg="", x=225, y=100, font_size=40):
 		self.render_options_screen()
-		lbl = Label(
-			self.window, text=label_msg, fg="black", font=("Helvetica", font_size)
-		)
-		self.elements_to_draw[lbl] = {
-			"x": x,
-			"y": y,
-		}
-		return self.elements_to_draw
+		self.__add_label(x=x, y=y, text=label_msg, fg="black", font_size=font_size)
+		return self.elems_to_draw
 
 	def set_screen(self, screen_type):
 		self.__clear_screen()
-		if screen_type == ScreenType.entry:
+		if screen_type == self.ScreenType.entry:
 			gui.render_entry_screen()
-		elif screen_type == ScreenType.invalid_card:
+		elif screen_type == self.ScreenType.invalid_card:
 			gui.render_invalid_card_screen()
-		elif screen_type == ScreenType.pin_code:
+		elif screen_type == self.ScreenType.pin_code:
 			gui.render_pincode_enter_screen()
-		elif screen_type == ScreenType.invalid_pin:
+		elif screen_type == self.ScreenType.invalid_pin:
 			gui.render_invalid_pin_screen()
-		elif screen_type == ScreenType.options:
+		elif screen_type == self.ScreenType.options:
 			gui.render_options_screen()
-		elif screen_type == ScreenType.options_with_label:
+		elif screen_type == self.ScreenType.options_with_label:
 			gui.render_options_with_label_screen()
 		else:
 			raise Exception("Wrong screen")
-		self.update()
-		return self.elements_to_draw
+		return self.elems_to_draw
 
 	def update(self):
-		for elem in self.elements_to_draw:
-			elem.place(x=self.elements_to_draw[elem]["x"], y=self.elements_to_draw[elem]["y"])
+		for elem in self.elems_to_draw:
+			elem.place(x=self.elems_to_draw[elem]["x"], y=self.elems_to_draw[elem]["y"])
 		self.window.update()
-
-
-
 
 
 if __name__ == "__main__":
 	gui = Gui(title="ATM simulator", win_size=510, bg_img="./resources/bg.png")
-	gui.set_screen(ScreenType.entry)
-
+	gui.set_screen(Gui.ScreenType.entry)
 	while True:
-
 		gui.update()
+
+
